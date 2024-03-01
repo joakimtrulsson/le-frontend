ff45f2950ad646be29aaa45fb0e73e3e6f1158ce
Joakim Trulsson <joakim.trulsson@icloud.com>
2024-03-01 12:51:48 +0100

    Added more logos

--- src/components/OurPartners.tsx
+++ src/components/OurPartners.tsx
@@ -7,6 +7,10 @@ import meWhiteLogo from '../assets/partners/me/me-transparent-white.png';
 import meGreyLogo from '../assets/partners/me/me-transparent-grey.png';
 import sekoWhiteLogo from '../assets/partners/seko/seko-white-transparent.png';
 import sekoGreyLogo from '../assets/partners/seko/seko-grey-transparent.png';
+import idGreyLogo from '../assets/partners/id06/id06-grey.png';
+import idWhiteLogo from '../assets/partners/id06/id06-white.png';
+import energyWhiteLogo from '../assets/partners/energi/energi-white.png';
+import energyGreyLogo from '../assets/partners/energi/energi-grey.png';
 
 interface OurPartnerProps {
   mode: PaletteMode;
@@ -14,6 +18,11 @@ interface OurPartnerProps {
 
 const logoStyle = {
   height: '40px',
+  margin: '0 32px',
+};
+
+const logoStyleID06 = {
+  height: '45px',
   margin: '0 32px',
 };
 
@@ -37,7 +46,7 @@ export default function OurParterns({ mode }: OurPartnerProps) {
         <Grid>
           <img
             src={mode === 'light' ? sekoGreyLogo : sekoWhiteLogo}
-            alt='me logo'
+            alt='seko logo'
             style={logoStyle}
           />
         </Grid>
@@ -46,6 +55,20 @@ export default function OurParterns({ mode }: OurPartnerProps) {
             src={mode === 'light' ? meGreyLogo : meWhiteLogo}
             alt='me logo'
             style={logoStyle}
+          />
+        </Grid>
+        <Grid sx={{ ml: { xs: '-55px', sm: 0 }, mt: { xs: 3, sm: 0 } }}>
+          <img
+            src={mode === 'light' ? energyGreyLogo : energyWhiteLogo}
+            alt='energiföretagen logo'
+            style={logoStyle}
+          />
+        </Grid>
+        <Grid sx={{ display: 'flex', mt: { xs: 3, sm: 0 } }}>
+          <img
+            src={mode === 'light' ? idGreyLogo : idWhiteLogo}
+            alt='id06 logo'
+            style={logoStyleID06}
           />
         </Grid>
       </Grid>
